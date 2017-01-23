65e546c560d424012d56ee951b9f6e804b8386c7
Rose Cox <rose@purpleprodigy.com>
2017-01-23 15:44:16 +1000

    Initial commit

---
+++ src/shortcode/shortcodes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description
+ *
+ * @package     NAMESPACE
+ * @since       1.0.0
+ * @author      Purple Prodigy
+ * @link        http://www.purpleprodigy.com
+ * @licence     GNU General Public License 2.0+
+ */
+namespace NAMESPACE
