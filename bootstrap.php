64d4a8959b53c17806cbb8f89ca605332355a53a
Rose Cox <rose@purpleprodigy.com>
2017-03-30 14:24:14 +1000

    Uptick plugin version

--- bootstrap.php
+++ bootstrap.php
@@ -10,9 +10,9 @@
 /*
  * @wordpress-plugin
  * Plugin Name:     Collapsible Content
- * Plugin URI:      https://github/
+ * Plugin URI:      https://github.com/purpleprodigy/CollapsibleContent
  * Description:     Collapsible Content is a WordPress Plugin that shows and hides hidden content.  Practical examples include Q&As, FAQs, hints, marketing teasers, and more.  Click the icon to open and reveal the content. Click again to close and hide it.
- * Version:         1.0.0
+ * Version:         1.2.0
  * Author:          Purple Prodigy
  * Author URI:      http://www.purpleprodigy.com
  * Text Domain:     collapsible_content
